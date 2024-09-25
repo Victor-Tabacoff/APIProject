5411e5be5744af019e280312817fb7b6a4021823
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-09-25 08:25:36 -0500

    Initial Commit

---
+++ APIProject/APIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIProjectApp.swift
+//  APIProject
+//
+//  Created by Victor B. Tabacoff on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct APIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIProjectTests/APIProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIProjectTests.swift
+//  APIProjectTests
+//
+//  Created by Victor B. Tabacoff on 9/25/24.
+//
+
+import Testing
+@testable import APIProject
+
+struct APIProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
